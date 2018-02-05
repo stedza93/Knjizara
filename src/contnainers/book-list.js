63ed52494c1b175204eb8e6c769423f0d2db1ae3
stedza93 <stefanmila93@gmail.com>
2018-02-05 06:58:04 +0100

    sve izmjene

--- src/contnainers/book-list.js
+++ src/contnainers/book-list.js
@@ -1,20 +1,26 @@
-import React, {Component} from 'react';
-export default class Booklist extends Component{
-renderList(){
-    return() this.props.books.map((book)) => {
-<li key={book.title} className="list-group-item"> {book.title}</li>
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-    }
+class Booklist extends Component {
+  renderList() {
+    return this.props.books.map(book => {
+      return (
+        <li key={book.title} className="list-group-item">
+          {" "}
+          {book.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
+  }
+}
+function mapStateToProps(state){
+  return{
+    books: state.books
+  };
 }
 
-render(){
-return(
-    <ul className="list-group col-sm-4">
-    {this.renderList()}
-    </ul>
-)
-
-}
-
-
-}
+export default connect(mapStateToProps)(Booklist);
