45d65019c3ba63e55018c35fbceb5c74afdf34d9
stedza93 <stefanmila93@gmail.com>
2018-02-05 15:41:29 +0100

    jos jedan komit :D

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Booklist from "../contnainers/book-list.js";
-
+import BookDetail from "../contnainers/book-detail";
 export default class App extends Component {
   render() {
     return (
       <div>
         <Booklist />
+        <BookDetail />
       </div>
     );
   }
---
+++ src/contnainers/book-detail.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class BookDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h3> Details for: </h3>
+        <div>{this.props.book.title}</div>
+      </div>
+    );
+  }
+}
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  };
+}
+export default connect(mapStateToProps)(BookDetail);
