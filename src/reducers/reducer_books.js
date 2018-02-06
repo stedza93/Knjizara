247588899ccd09d9a2100b5c6fc921213ec9560d
stedza93 <stefanmila93@gmail.com>
2018-02-06 07:14:20 +0100

    zavrsena knjizara

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: "Javascript:The Good Parts" },
-    { title: "Harry Potter" },
-    { title: "The Dark Tower" },
-    { title: "Robin Hood" }
+    { title: "Javascript:The Good Parts", pages: 101 },
+    { title: "Harry Potter", pages: 101 },
+    { title: "The Dark Tower", pages: 101 },
+    { title: "Robin Hood", pages: 101 }
   ];
 }
